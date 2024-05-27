2bf7efa34fe0b9b24b17cec7a2b0c0d63c892638
MD Rehan <mdrehan4650@gmail.com>
2024-05-28 00:15:53 +0530

    Added env in client

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // '/api': 'http://localhost:3000'
+        '/api': env.VITE_BACKEND_URL
+      }
     }
   }
 })
